48e2f574f7ecae5e3b8ae4082925e8a4eb24d934
matt wardlow <mwardlow0@gmail.com>
2022-08-21 03:32:02 -0400

    hosting

--- index.js
+++ index.js
@@ -33,15 +33,14 @@ mongoose.connect(process.env.MONGO_URI, {
 })
     .then(() => {
         console.log('DB Connection Succesfull');
-
-        app.listen(process.env.PORT, () => {
-          console.log(`Server running on port ${process.env.PORT}`)
-        })
     })
     .catch((err) => {
         console.log(err.message)
     })
 
+    const server = app.listen(process.env.PORT, () => {
+      console.log(`Server Started on Port ${process.env.PORT}`)
+  })
 
 
 
