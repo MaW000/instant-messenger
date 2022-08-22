5a670073e7603c92d9d6f04e269c98ab45e9c414
matt wardlow <mwardlow0@gmail.com>
2022-08-22 19:17:02 -0400

    hosting

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
-export const host = 'http://localhost:5000'
+
+export const host = `http://localhost:${process.env.REACT_APP_LOCALHOST_KEY}`
 export const registerRoute = `${host}/auth/register`
 export const loginRoute = `${host}/auth/login` 
 export const allUsersRoute = `${host}/api/allusers`
