9272323566f7b1b731b83875334aa6bf59b0f7ee
matt wardlow <mwardlow0@gmail.com>
2022-08-22 19:30:19 -0400

    hosting

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,11 +1,10 @@
 
-export const host = `http://localhost:${process.env.REACT_APP_LOCALHOST_KEY}`
-export const registerRoute = `${host}/auth/register`
-export const loginRoute = `${host}/auth/login` 
-export const allUsersRoute = `${host}/api/allusers`
-export const setAvatarRoute = `${host}/api/setAvatar`
-export const sendMessageRoute = `${host}/api/addmsg`
-export const getMessagesRoute = `${host}/api/getmsg`
-export const createServerRoute = `${host}/api/createserver`
-export const getServerRoute = `${host}/api/getserver`
-export const getMessageServerRoute = `${host}/api/getservermsg`
+export const registerRoute = `/auth/register`
+export const loginRoute = `/auth/login` 
+export const allUsersRoute = `/api/allusers`
+export const setAvatarRoute = `/api/setAvatar`
+export const sendMessageRoute = `/api/addmsg`
+export const getMessagesRoute = `/api/getmsg`
+export const createServerRoute = `/api/createserver`
+export const getServerRoute = `/api/getserver`
+export const getMessageServerRoute = `/api/getservermsg`
